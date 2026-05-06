2351f0f0ec5d748f99e9c93b286d6a8abb627e1d
Pixel Brush <letsplaytvirtmann@gmail.com>
2026-05-06 22:50:30 +0200

    Some more stuff

--- README.md
+++ README.md
@@ -27,12 +27,17 @@ Just run the `build.sh` file, then use something like `python3 -m http.server` t
 
 - Beta 1.3.0 - Beta 1.7.3 (True Beta-era)
 - Alpha v1.2.0 - Beta 1.2.0_02 (Early Beta-era, [Halloween Update terrain generator](https://minecraft.wiki/w/World_generation/History#Halloween_Update_terrain_generator))
-- Infdev 20100611 - Alpha 1.1.2_01 (Alpha-era terrain, Monoliths)
+- Infdev 20100624 - Alpha 1.1.2_01 (Alpha-era terrain, Monoliths)
 - Infdev 20100327 (first version with the Farlands)
 - Infdev 20100227-1433 (brick pyramids + obsidian wall)
 
 ## Planned generators
 
-- ~~A few more of the Infdev and Alpha generators~~ ✅
+- A few more of the ~~~~Alpha generators~~ ✅
+- A few more of the Infdev generators
+    - [ ] Erosion (20100413 - 20100415)
+    - [ ] Big change, more hills and cliffs (20100420 - 20100608)
+    - [ ] Large islands, high amplification (20100611 - 20100615)
+    - [ ] More gradual coastlines (20100616 - 20100618)
 - Beta 1.8 and Beta 1.9 Generators
 - Early Release Generators
--- dist/map.js
+++ dist/map.js
@@ -154,9 +154,13 @@ window.addEventListener('load', () => {
                             <select id="genSelection">
                                 <option value="3">b1.3.0 - b1.7.3</option>
                                 <option value="5">a1.2.0 - b1.2.0_02</option>
-                                <option value="4">inf-20100611 - a1.1.2_01</option>
-                                <option value="2">inf-20100327</option>
-                                <option value="1">inf-20100227</option>
+                                <option value="4">inf-20100624 - a1.1.2_01</option>
+                                <option value="9" disabled="true">inf-20100616 - inf-20100618</option>
+                                <option value="8" disabled="true">inf-20100611 - inf-20100615</option>
+                                <option value="7" disabled="true">inf-20100420 - inf-20100608</option>
+                                <option value="6" disabled="true">inf-20100413 - inf-20100415</option>
+                                <option value="2">inf-20100327 - inf-20100330</option>
+                                <option value="1">inf-20100227 - inf-20100325</option>
                             </select>
                         </td>
                     </tr>
